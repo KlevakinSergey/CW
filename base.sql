d5d5266fe75803e4fc140ab308dac382b069f000
KlevakinSergey <klevakin.sergey@gmail.com>
2020-02-03 22:57:22 +0600

    CW final

--- base.sql
+++ base.sql
@@ -4,16 +4,91 @@
 -- ------------------------------------------------------
 -- Server version	5.5.48
 
+/
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `laptop`
+--
+
+DROP TABLE IF EXISTS `laptop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `laptop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `developer_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `developer_id` (`developer_id`),
+  CONSTRAINT `developer_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `developer` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laptop`
+--
+
+LOCK TABLES `laptop` WRITE;
+/*!40000 ALTER TABLE `laptop` DISABLE KEYS */;
+INSERT INTO `laptop` VALUES (1,1),(2,2),(3,3),(4,4);
+/*!40000 ALTER TABLE `laptop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_manager`
+--
+
+DROP TABLE IF EXISTS `project_manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project_manager` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_manager`
+--
+
+LOCK TABLES `project_manager` WRITE;
+/*!40000 ALTER TABLE `project_manager` DISABLE KEYS */;
+INSERT INTO `project_manager` VALUES (1,'Petr'),(2,'Igor');
+/*!40000 ALTER TABLE `project_manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'parser'),(2,'bot);
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `developer`
@@ -23,16 +98,15 @@ DROP TABLE IF EXISTS `developer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `developer` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `data_of_birth` date DEFAULT NULL,
-  `gender` varchar(20) DEFAULT NULL,
-  `manager_id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `sex` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `project_manader_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `manager_id` (`manager_id`),
-  CONSTRAINT `developer_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `project_manage` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `project_manader_id` (`project_manader_id`),
+  CONSTRAINT `razrab_ibfk_1` FOREIGN KEY (`project_manader_id`) REFERENCES `project_manader` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,5 +115,56 @@ CREATE TABLE `developer` (
 
 LOCK TABLES `developer` WRITE;
 /*!40000 ALTER TABLE `developer` DISABLE KEYS */;
+INSERT INTO `developer` VALUES (1,'Sergey','MALE','1999-09-01',1),(2,'sveta','FEMALE','1990-01-11',2),(3,'igor','MALE','2011-12-12',2),(4,'Beka','FEMALE','1980-02-08',2),(5,'DK','MALE','1990-02-03',1);
 /*!40000 ALTER TABLE `developer` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `developer_projects`
+--
+
+DROP TABLE IF EXISTS `developer_projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `developer_projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `developer_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `developer_id` (`developer_id`),
+  KEY `project_id` (`project_id`),
+  CONSTRAINT `developer_projects_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `developer` (`id`),
+  CONSTRAINT `developer_projects_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `developer_projects`
+--
+
+LOCK TABLES `developer_projects` WRITE;
+/*!40000 ALTER TABLE `developer_projects` DISABLE KEYS */;
+INSERT INTO `developer_projects` VALUES (1,1,1),(2,1,2),(3,2,2),(4,3,2),(5,4,2);
+/*!40000 ALTER TABLE `developer_projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping events for database 'ITcompany'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-03 21:52:03
+
+
+
+
+
